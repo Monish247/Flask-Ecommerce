11854e4eab37e3d46fdf2c69ab7cfa3e730be34c
Monish247 <84516022+Monish247@users.noreply.github.com>
2024-10-28 16:22:35 +0530

    Update startcontainer.sh

--- scripts/startcontainer.sh
+++ scripts/startcontainer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e 
-docker pull docker pull monish247/ecommerce_python_image
+docker pull monish247/ecommerce_python_image
 docker run -itd -p 8034:80 monish247/ecommerce_python_image:latest
